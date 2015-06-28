af597722307b2fdbc8d005f45d5301b881abe617
John Clune <jmclune@gmail.com>
2015-06-28 16:19:22 -0400

    Replaced emails with names.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => {:medium => "300x300", :thumb => "100x100"}
+  validates :description, presence: true
+  validates :image, presence: true
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
